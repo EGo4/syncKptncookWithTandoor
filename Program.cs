efb0383baa11098baff1adfd184c019a5365376f
Dexox <eric.gorkow@gmx.de>
2022-10-22 22:08:34 +0200

    moving files from private repo

---
+++ Program.cs
@@ -0,0 +1,269 @@
+﻿using System.Net.Http.Headers;
+using System.Text.Json.Nodes;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System.Net;
+using System.Linq.Expressions;
+using fetchkptncook;
+using fetchkptncook.Api;
+using fetchkptncook.Model;
+using fetchkptncook.Client;
+using System.Collections.Generic;
+using NPOI.POIFS.Crypt.Dsig;
+using System.IO;
+using System;
+using System.Text.RegularExpressions;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        Console.WriteLine("Enter KptnCook email:");
+        string username = Console.ReadLine();
+        Console.WriteLine("Enter password:");
+        string password = Console.ReadLine();
+        Console.WriteLine("Enter Tandor server URL:");
+        string url = Console.ReadLine();
+        Console.WriteLine("Enter Tandor Token:");
+        string apiKey = $"Bearer {Console.ReadLine()}";
+
+        Console.WriteLine("Login to KptnCook.");
+        string[] favorites = await login(username, password);
+
+        Console.WriteLine("Fetching recipes from KptnCook.");
+        List<Root> recipes = new List<Root>();
+        foreach(string favoId in favorites)
+            recipes.Add(await getRecipe(favoId));
+
+        Console.WriteLine($"Found {recipes.Count} recipes. Comparing with Tandor.");
+        int indexRecipe = 0;
+        while(indexRecipe < recipes.Count)
+            if (await checkRecipeExists(recipes[indexRecipe], apiKey, url))
+                recipes.RemoveAt(indexRecipe);
+            else
+                indexRecipe++;
+
+        Console.WriteLine($"Found {recipes.Count} new recipes.");
+
+        if (recipes.Count != 0)
+        {
+            Console.Write("Processing recipes, this may take a while: ");
+            int iUpload = 0;
+            foreach (Root recipe in recipes)
+            {
+                await importRecipe(recipe, apiKey, url);
+                iUpload++;
+                if (Math.Floor(20.0 * iUpload / recipes.Count) > Math.Floor(20.0 * (iUpload - 1) / recipes.Count))
+                    Console.Write("█");
+            }               
+        }
+        Console.WriteLine("Done syncronising KptnCook with Tandor.");
+
+    }
+
+    private static ApiApi getTandorApi(string apiKey, string url)
+    {
+        Configuration configuration = new Configuration();
+        IDictionary<string, string> dict = new Dictionary<string, string>();
+        dict.Add("Authorization", apiKey);
+        configuration.DefaultHeader = dict;
+        configuration.BasePath = url;
+        return new ApiApi(configuration);
+    }
+
+    private static async Task<bool> checkRecipeExists(Root recipe, string apiKey, string url)
+    {
+        ApiApi tandorApi = getTandorApi(apiKey, url);
+        // get list of existing recipes
+        List<RecipeOverview> recipesTandor = tandorApi.ListRecipes().Results ?? new List<RecipeOverview>();
+        // check if recipe is in existing recipes
+        return recipesTandor.Any(recipeTandor => recipeTandor.Name == recipe.title);
+    }
+
+    private static async Task importRecipe(Root recipe, string api_key, string url)
+    {
+        ApiApi tandorApi = getTandorApi(api_key, url);
+        // components of the tandor recipe
+        string name = recipe.title;
+        string description = recipe.authorComment;
+        List<RecipeKeywordsInner> keywords = new List<RecipeKeywordsInner>();
+        recipe.activeTags.ForEach(tag => keywords.Add(new RecipeKeywordsInner(tag, null, "")));
+        bool _internal = true;
+        
+        fetchkptncook.Model.RecipeNutrition recipeNutrition = new fetchkptncook.Model.RecipeNutrition(
+            recipe.recipeNutrition.carbohydrate.ToString(),
+            recipe.recipeNutrition.fat.ToString(),
+            recipe.recipeNutrition.protein.ToString(),
+            recipe.recipeNutrition.calories.ToString()
+        );
+        int workingTime = recipe.preparationTime;
+        int waitingTime = recipe.cookingTime ?? 0;
+        int servings = 1;
+        string filePath = "";
+
+        List<RecipeStepsInner> steps = new List<RecipeStepsInner>();
+        int i = 0;
+        foreach(Step thisStep in recipe.steps)
+        {
+            // line 74-85
+            // Parse the time and edit instructions accordingly
+            int time = 0;
+            string instructions = thisStep.title;
+
+            foreach(Timer timer in thisStep.timers)
+            {
+                time += timer.minOrExact;
+                Regex regex = new Regex(Regex.Escape("<timer>"));
+                instructions = regex.Replace(instructions, 
+                    $"{timer.minOrExact} -{(timer.max is null ? " " : timer.max.ToString())}min", 1);
+            }
+
+
+            List<RecipeStepsInnerIngredientsInner> recipeStepsInnerIngredientsInners = new List<RecipeStepsInnerIngredientsInner>();
+            // line 90 - 127
+            int orderIng = 0;
+            if (thisStep.ingredients != null)
+                foreach(Ingredient ingredient in thisStep.ingredients)
+                {
+                    recipeStepsInnerIngredientsInners.Add(
+                        new RecipeStepsInnerIngredientsInner(
+                            new IngredientFood(
+                                Encoding.UTF8.GetString(Encoding.Default.GetBytes(ingredient.title)),
+                                Encoding.UTF8.GetString(Encoding.Default.GetBytes(ingredient.title)),
+                                null,
+                                "false",
+                                null,
+                                new List<FoodInheritFieldsInner>(),
+                                false,
+                                new List<FoodSubstituteInner>(),
+                                false,
+                                false,
+                                new List<FoodInheritFieldsInner>()
+                            ),
+                            ingredient.unit != null ? ingredient.unit.measure != null ?
+                            // line 115,´120, 121
+                                new FoodSupermarketCategory(ingredient.unit.metricMeasure) : null :  null,
+                            // 114 and 118 and 123
+                            ingredient.unit != null ? ingredient.unit.metricQuantity.ToString() : ingredient.quantity != null ? ingredient.metricQuantity.ToString() : "",
+                            "",
+                            orderIng,
+                            false,
+                            ingredient.unit == null ? ingredient.quantity == null ? true : false : false,
+                            null
+                        )
+                    );
+                    orderIng++;
+                }
+
+            //line 61-72
+            string imgUrl = thisStep.image.url;
+            FileStream imgData = await getImage(imgUrl);
+            string[] imgName = System.IO.Path.GetFileName(imgData.Name).Split('.');
+
+            UserFile imageResponse = await tandorApi.CreateUserFileAsync(imgName[0], imgData);
+            imgData.Close();
+            // line 131
+            steps.Add(
+                new RecipeStepsInner(
+                    "",
+                    instructions,
+                    recipeStepsInnerIngredientsInners,
+                    time,
+                    i,
+                    true,
+                    imageResponse.ToRecipeStepsInnerFile()
+                )
+            );
+            i++;
+        }
+
+        Recipe tandorRecipe = new Recipe(name, description, keywords, steps, workingTime, waitingTime, null, _internal, true,
+            recipeNutrition, servings, filePath, "", false, new List<CustomFilterSharedInner>());
+        // line 137
+        Recipe responseTandor = await tandorApi.CreateRecipeAsync(tandorRecipe);
+
+        // line 149
+        string? coverImgUrl = null;
+        foreach (ImageList img in recipe.imageList)
+            if(img.type == "cover")
+            {
+                coverImgUrl = img.url;
+                break;
+            }
+
+        if(coverImgUrl != null)
+        {
+            FileStream imgData = await getImage(coverImgUrl);
+            RecipeImage recipeImage = tandorApi.ImageRecipe(responseTandor.Id.ToString(), imgData);
+            imgData.Close();
+        }       
+        // End = line 170
+
+    }
+
+    private static async Task<string[]> login(string username, string password)
+    {
+        HttpClientHandler httpClientHandler = new HttpClientHandler() { AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate };
+        HttpClient client = new HttpClient(httpClientHandler);
+
+        client.BaseAddress = new Uri("https://mobile.kptncook.com:443");
+
+        var request = new HttpRequestMessage(System.Net.Http.HttpMethod.Post, new Uri("https://mobile.kptncook.com:443/login/userpass"));
+        request.Content = new StringContent($"{{\"email\": \"{username}\", \"password\": \"{password}\"}}", Encoding.ASCII, "application/json");
+
+        HttpResponseMessage response = await client.SendAsync(request);
+
+        loginResponse? resp = JsonConvert.DeserializeObject<loginResponse>(await response.Content.ReadAsStringAsync());
+        string json = await response.Content.ReadAsStringAsync();
+        if (resp != null)
+            return resp.favorites;
+        else
+            return new string[] {""};
+    }
+
+    private static async Task<FileStream> getImage(string imgUrl)
+    {
+        HttpClientHandler httpClientHandler = new HttpClientHandler();
+        HttpClient client = new HttpClient(httpClientHandler);
+
+        client.DefaultRequestHeaders.Add("kptnkey", "6q7QNKy-oIgk-IMuWisJ-jfN7s6");
+
+        Stream response = await client.GetStreamAsync(imgUrl);
+
+        var debResponse = await client.GetAsync(imgUrl);
+        string testString = await debResponse.Content.ReadAsStringAsync();
+        // put stream together
+        FileStream stream = new FileStream($"{Guid.NewGuid().ToString()}.png", FileMode.Create, FileAccess.ReadWrite, FileShare.None, 4069, FileOptions.DeleteOnClose);
+        debResponse.Content.ReadAsStream().CopyTo(stream);
+        stream.Position = 0;
+
+        return stream; 
+
+    }
+
+    private static async Task<Root> getRecipe(string id)
+    {
+        HttpClientHandler httpClientHandler = new HttpClientHandler() { AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate };
+        HttpClient client = new HttpClient(httpClientHandler);
+
+        client.BaseAddress = new Uri("https://mobile.kptncook.com:443");
+        client.DefaultRequestHeaders.Add("hasIngredients", "YES");
+        client.DefaultRequestHeaders.Add("kptnkey", "6q7QNKy-oIgk-IMuWisJ-jfN7s6");
+        client.DefaultRequestHeaders.Add("Accept", "application/vnd.kptncook.mobile-v8+json");
+        client.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", "Platform/Android/5.0.1 App/7.2.7");
+        client.DefaultRequestHeaders.Add("Accept-Encoding", "gzip, deflate");
+        client.DefaultRequestHeaders.Add("Connection", "keep-alive");
+
+        var request = new HttpRequestMessage(System.Net.Http.HttpMethod.Post, new Uri("https://mobile.kptncook.com:443/recipes/search?lang=de&store=de"));
+        request.Content = new StringContent("[{ \"identifier\":\"" + id + "\"}]", Encoding.ASCII, "application/json");
+
+        HttpResponseMessage response = await client.SendAsync(request);
+
+        string? responseString = await response.Content.ReadAsStringAsync();
+        List<Root>? resp = JsonConvert.DeserializeObject<List<Root>>(responseString);
+
+        return resp.First() ?? new Root();
+    }
+}
---
+++ ResponseObjects.cs
@@ -0,0 +1,271 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class loginResponse
+{
+        public string accessToken { get; set; }
+        public string name { get; set; }
+        public int favspace { get; set; }
+        public string[] favorites { get; set; }
+        public string inviteCode { get; set; }
+}
+
+// Root myDeserializedClass = JsonConvert.DeserializeObject<List<Root>>(myJsonResponse);
+public class Author
+{
+    public Id _id { get; set; }
+    public string name { get; set; }
+    public string link { get; set; }
+    public string title { get; set; }
+    public string description { get; set; }
+    public string sponsor { get; set; }
+    public AuthorImage authorImage { get; set; }
+    public CreationDate creationDate { get; set; }
+    public UpdateDate updateDate { get; set; }
+}
+
+public class AuthorImage
+{
+    public string name { get; set; }
+    public string url { get; set; }
+}
+
+public class Brand
+{
+    public string id { get; set; }
+    public string name { get; set; }
+    public List<string> countries { get; set; }
+    public object ingredientTitle { get; set; }
+    public object uncountableTitle { get; set; }
+    public IngredientImage ingredientImage { get; set; }
+}
+
+public class CreationDate
+{
+    [JsonProperty("$date")]
+    public long Date { get; set; }
+}
+
+public class De
+{
+    [JsonProperty("$date")]
+    public long Date { get; set; }
+}
+
+public class En
+{
+    [JsonProperty("$date")]
+    public long Date { get; set; }
+}
+
+public class Id
+{
+    [JsonProperty("$oid")]
+    public string Oid { get; set; }
+}
+
+public class Image
+{
+    public string name { get; set; }
+    public string url { get; set; }
+    public string type { get; set; }
+}
+
+public class ImageList
+{
+    public string name { get; set; }
+    public string url { get; set; }
+    public string type { get; set; }
+}
+
+public class Ingredient
+{
+    public Unit unit { get; set; }
+    public string ingredientId { get; set; }
+    public string title { get; set; }
+    public NumberTitle numberTitle { get; set; }
+    public Ingredient ingredient { get; set; }
+    public double? quantity { get; set; }
+    public double? metricQuantity { get; set; }
+    public double? quantityUS { get; set; }
+    public double? imperialQuantity { get; set; }
+    public double? quantityUSProd { get; set; }
+    public double? imperialProductQuantity { get; set; }
+    public string measure { get; set; }
+    public string metricMeasure { get; set; }
+    public string measureUS { get; set; }
+    public string imperialMeasure { get; set; }
+    public string measureUSProd { get; set; }
+    public string imperialProductMeasure { get; set; }
+}
+
+public class Ingredient3
+{
+    public Id _id { get; set; }
+    public string typ { get; set; }
+    public string title { get; set; }
+    public NumberTitle numberTitle { get; set; }
+    public string uncountableTitle { get; set; }
+    public string category { get; set; }
+    public string key { get; set; }
+    public string desc { get; set; }
+    public Image image { get; set; }
+    public List<Product> products { get; set; }
+    public bool isSponsored { get; set; }
+    public Measures measures { get; set; }
+    public List<Brand> brands { get; set; }
+    public CreationDate creationDate { get; set; }
+    public UpdateDate updateDate { get; set; }
+    public string note { get; set; }
+}
+
+public class IngredientImage
+{
+    public string name { get; set; }
+    public string url { get; set; }
+}
+
+public class LocalizedPublishDate
+{
+    public En en { get; set; }
+    public De de { get; set; }
+}
+
+public class Measures
+{
+    public List<string> de { get; set; }
+    public List<string> us { get; set; }
+}
+
+public class NumberTitle
+{
+    public string singular { get; set; }
+    public string plural { get; set; }
+}
+
+public class Product
+{
+    public Id _id { get; set; }
+    public string title { get; set; }
+    public string ingredient { get; set; }
+    public string retailer { get; set; }
+    public double price { get; set; }
+    public int quantity { get; set; }
+    public double newQuantity { get; set; }
+    public string measure { get; set; }
+    public CreationDate creationDate { get; set; }
+    public UpdateDate updateDate { get; set; }
+    public string cbid { get; set; }
+}
+
+public class PublishDates
+{
+    public List<En> en { get; set; }
+    public List<De> de { get; set; }
+}
+
+public class PublishDuration
+{
+    public int en { get; set; }
+    public int de { get; set; }
+}
+
+public class RecipeNutrition
+{
+    public int calories { get; set; }
+    public int protein { get; set; }
+    public int fat { get; set; }
+    public int carbohydrate { get; set; }
+}
+
+public class RecipeRating
+{
+    public int likeCount { get; set; }
+    public int dislikeCount { get; set; }
+}
+
+public class Retailer
+{
+    public Id _id { get; set; }
+    public string rkey { get; set; }
+    public string key { get; set; }
+    public string name { get; set; }
+    public string status { get; set; }
+    public string typ { get; set; }
+    public string country { get; set; }
+    public string priceUpdate { get; set; }
+    public string mapStatus { get; set; }
+    public bool onlineOrderingState { get; set; }
+    public CreationDate creationDate { get; set; }
+    public UpdateDate updateDate { get; set; }
+    public double? discount { get; set; }
+}
+
+public class Root
+{
+    public Id _id { get; set; }
+    public string title { get; set; }
+    public string rtype { get; set; }
+    public string gdocs { get; set; }
+    public string authorComment { get; set; }
+    public string uid { get; set; }
+    public string country { get; set; }
+    public int preparationTime { get; set; }
+    public int? cookingTime { get; set; }
+    public RecipeNutrition recipeNutrition { get; set; }
+    public List<string> activeTags { get; set; }
+    public List<Step> steps { get; set; }
+    public List<Author> authors { get; set; }
+    public List<Retailer> retailers { get; set; }
+    public List<Ingredient> ingredients { get; set; }
+    public List<ImageList> imageList { get; set; }
+    public LocalizedPublishDate localizedPublishDate { get; set; }
+    public string trackingMode { get; set; }
+    public string feature { get; set; }
+    public PublishDuration publishDuration { get; set; }
+    public int favoriteCount { get; set; }
+    public bool modularizationEnabled { get; set; }
+    public PublishDates publishDates { get; set; }
+    public bool onlineOrderingIsActive { get; set; }
+    public RecipeRating recipeRating { get; set; }
+    public CreationDate creationDate { get; set; }
+    public UpdateDate updateDate { get; set; }
+}
+
+public class Step
+{
+    public string title { get; set; }
+    public Image image { get; set; }
+    public List<Timer> timers { get; set; }
+    public List<Ingredient> ingredients { get; set; }
+}
+
+public class Timer
+{
+    public int minOrExact { get; set; }
+    public int? max { get; set; }
+}
+
+public class Unit
+{
+    public double metricQuantity { get; set; }
+    public double imperialQuantity { get; set; }
+    public double quantity { get; set; }
+    public double quantityUS { get; set; }
+    public string metricMeasure { get; set; }
+    public string imperialMeasure { get; set; }
+    public string measure { get; set; }
+    public string measureUS { get; set; }
+}
+
+public class UpdateDate
+{
+    [JsonProperty("$date")]
+    public long Date { get; set; }
+}
+
